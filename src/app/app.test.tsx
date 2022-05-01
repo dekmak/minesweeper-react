b633fda26291b8ca08b4c10957dbced6206ee5df
Ahmad Dekmak <ahmeddekmak@gmail.com>
2022-05-01 17:16:30 +0300

    Manage tests

--- src/app/app.test.tsx
+++ src/app/app.test.tsx
@@ -6,7 +6,6 @@ import {mount} from 'enzyme';
 import {cleanup, render} from '@testing-library/react';
 import configureStore from 'redux-mock-store';
 import createSagaMiddleware from 'redux-saga';
-import {watcherSaga} from '../store/sagas/rootSaga';
 
 const sagaMiddleware = createSagaMiddleware();
 const mockStore = configureStore([sagaMiddleware]);
@@ -15,23 +14,6 @@ describe('<App />', () => {
   afterEach(() => {
     jest.clearAllMocks();
     cleanup();
-  });
-  it('renders correctly and to match snapshot', () => {
-    const initialState = {
-      game: {
-        map: [],
-        message: '',
-      },
-    };
-    const store = mockStore(initialState);
-    const tree = renderer
-        .create(
-            <Provider store={store}>
-              <App/>
-            </Provider>,
-        )
-        .toJSON();
-    expect(tree).toMatchSnapshot();
   });
   it('Start button appears in first render', () => {
     const initialState = {
@@ -47,36 +29,7 @@ describe('<App />', () => {
         </Provider>,
     );
     expect(wrapper.text().includes('Minesweeper')).toBe(true);
-    expect(wrapper.text().includes('Press START and start playing Minesweeper!')).toBe(true);
+    expect(wrapper.text().includes('Press START to play!')).toBe(true);
     expect(wrapper.text().includes('Start')).toBe(true);
-  });
-  it('Start button appears in first render', async () => {
-    const initialState = {
-      game: {
-        map: [
-          '□□□□□□□□□□',
-          '□□□□□□□□□□',
-          '□□□□□□□□□□',
-          '□□□□□□□□□□',
-          '□□□□□□□□□□',
-          '□□□□□□□□□□',
-          '□□□□□□□□□□',
-          '□□□□□□□□□□',
-          '□□□□□□□□□□',
-          '□□□□□□□□□□',
-        ],
-        message: '',
-      },
-    };
-    const store = mockStore(initialState);
-    sagaMiddleware.run(watcherSaga);
-
-    const wrapper = render(
-        <Provider store={store}>
-          <App />
-        </Provider>,
-    );
-
-    expect(wrapper.getByText('Play again')).toBeTruthy();
   });
 });
--- src/game/gameTable.test.tsx
+++ src/game/gameTable.test.tsx
@@ -9,23 +9,6 @@ describe('<GameTable />', () => {
     jest.clearAllMocks();
     cleanup();
   });
-  it('renders correctly and to match snapshot', () => {
-    const gameMap: string[] = [];
-    const tree = renderer
-        .create(
-            <GameTable gameMap={gameMap}/>,
-        )
-        .toJSON();
-    expect(tree).toMatchSnapshot();
-  });
-
-  it('should render empty state correctly', () => {
-    const gameMap: string[] = [];
-    const wrapper = mount(
-        <GameTable gameMap={gameMap}/>,
-    );
-    expect(wrapper.text().includes('Press START and start playing Minesweeper!')).toBe(true);
-  });
   it('should find at least 1 square cell', () => {
     const gameMap: string[] = [
       '□□□□□□□□□□',
