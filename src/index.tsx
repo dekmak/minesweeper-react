069588af2cf46a13ae32b4f7e438941641346d93
Ahmad Dekmak <ahmeddekmak@gmail.com>
2022-05-01 16:49:05 +0300

    Fix React

--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,20 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 import './index.css';
 import 'antd/dist/antd.css';
 import App from './app/app';
 import {store} from './store/store';
 import {Provider} from 'react-redux';
 import * as serviceWorker from './serviceWorker';
+import {createRoot} from 'react-dom/client';
 
-ReactDOM.render(
+const container = document.getElementById('root') as HTMLElement;
+const root = createRoot(container);
+root.render(
     <React.StrictMode>
       <Provider store={store}>
         <App />
       </Provider>
     </React.StrictMode>,
-    document.getElementById('root'),
 );
 
 // If you want your app to work offline and load faster, you can change
